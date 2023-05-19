114c3fb551bb57ab05cf5deebdbc8495b5555d30
Nata2303 <121019039+Nata2303@users.noreply.github.com>
2023-05-19 18:57:24 +0000

    Deploying to gh-pages from @ Nata2303/goit-js-hw-09@f2328f2306bd0c613b57786813b9d76b2608bb4b 🚀

---
+++ 01-color-switcher.5ebb45fc.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=null;t.addEventListener("click",(function(){n||(t.disabled=!0,n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"))}),1e3))})),e.addEventListener("click",(function(){clearInterval(n),n=null,t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.5ebb45fc.js.map
---
+++ 01-color-switcher.a1da1ca2.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let a=null;t.addEventListener("click",(()=>{a||(t.disabled=!0,a=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`}),1e3))})),e.addEventListener("click",(()=>{clearInterval(a),a=null,t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.a1da1ca2.js.map
